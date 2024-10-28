3b940c856a54b469a9199a85794f4dc6fe5288f7
shapoco <40152459+shapoco@users.noreply.github.com>
2024-10-29 00:28:51 +0900

    delete bar led

--- src/core1.cpp
+++ src/core1.cpp
@@ -3,121 +3,49 @@
 
 #include "core0.hpp"
 #include "core1.hpp"
+
 #include "ssd1309spi.hpp"
+using pen_t = ssd1309spi::pen_t;
 
 extern volatile uint8_t glb_jjy_curr_signal;
 extern volatile uint8_t glb_jjy_qty_percent;
-
-static uint8_t vram[BARLED_NUM_SEGS];
-
-static constexpr int SCAN_INTERVAL_US = 200;
-
-static void animate_bit_scan(uint64_t t_now);
 
 static constexpr int LCD_W = 128;
 static constexpr int LCD_H = 64;
 
 static constexpr int LCD_SPI_INDEX = 0;
-static constexpr uint32_t LCD_SPI_FREQ = 100e3;
+static constexpr uint32_t LCD_SPI_FREQ = 1e6;
 static constexpr int PIN_LCD_CS_N = 17;
 static constexpr int PIN_LCD_SCLK = 18;
 static constexpr int PIN_LCD_MOSI = 19;
 static constexpr int PIN_LCD_RES_N = 20;
 static constexpr int PIN_LCD_DC = 21;
 
-Ssd1309Spi<LCD_W, LCD_H, LCD_SPI_INDEX, LCD_SPI_FREQ, PIN_LCD_RES_N, PIN_LCD_CS_N, PIN_LCD_DC, PIN_LCD_SCLK, PIN_LCD_MOSI> lcd;
+ssd1309spi::Lcd<LCD_W, LCD_H, LCD_SPI_INDEX, LCD_SPI_FREQ, PIN_LCD_RES_N, PIN_LCD_CS_N, PIN_LCD_DC, PIN_LCD_SCLK, PIN_LCD_MOSI> lcd;
 
 void core1_init() {
-    for (int seg = 0; seg < BARLED_NUM_SEGS; seg++) {
-        vram[seg] = 0;
-    }
-    for (int seg = 0; seg < BARLED_NUM_SEGS; seg++) {
-        gpio_init(PIN_BARLED_SEG_BASE + seg);
-        gpio_put(PIN_BARLED_SEG_BASE + seg, false);
-        gpio_set_dir(PIN_BARLED_SEG_BASE + seg, GPIO_IN);
-    }
-    for (int ch = 0; ch < BARLED_NUM_CHS; ch++) {
-        gpio_init(PIN_BARLED_CH_BASE + ch);
-        gpio_put(PIN_BARLED_CH_BASE + ch, false);
-        gpio_set_dir(PIN_BARLED_CH_BASE + ch, GPIO_OUT);
-    }
     lcd.init();
 }
 
 void core1_main() {
-    int seg = 0;
-    uint64_t t_next_upd = to_us_since_boot(get_absolute_time()) + SCAN_INTERVAL_US;
+    uint32_t t_next_flip_ms = to_ms_since_boot(get_absolute_time());
+    float x = LCD_W / 2;
+    float y = LCD_H / 2;
+    float dx = 1.0 / 10;
+    float dy = 0.99999 / 10;
     while (true) {
-        uint8_t c = vram[BARLED_NUM_SEGS - seg - 1];
-        gpio_put(PIN_BARLED_CH_BASE + BARLED_CH_R, !(c & 1));
-        gpio_put(PIN_BARLED_CH_BASE + BARLED_CH_G, !(c & 2));
-        gpio_put(PIN_BARLED_CH_BASE + BARLED_CH_B, !(c & 4));
-        gpio_put(PIN_BARLED_SEG_BASE + seg, true);
-        gpio_set_dir(PIN_BARLED_SEG_BASE + seg, GPIO_OUT);
-        
-        uint64_t t_now_us;
-        while ((t_now_us = to_us_since_boot(get_absolute_time())) < t_next_upd) {
-            animate_bit_scan(t_now_us);
-            sleep_us(10);
+        uint32_t t_now_ms = to_ms_since_boot(get_absolute_time());
+        if (t_now_ms >= t_next_flip_ms) {
+            t_next_flip_ms = t_now_ms + 10;
+            //lcd.set_pixel(x, y);
+            //lcd.clear();
+            lcd.fill_rect(x - 15, y - 15, 30, 30, (t_now_ms >> 9) & 1 ? pen_t::WHITE : pen_t::BLACK);
+            x += dx;
+            y += dy;
+            if ((x < 0 && dx < 0) || (x >= LCD_W && dx > 0)) dx = -dx;
+            if ((y < 0 && dy < 0) || (y >= LCD_H && dy > 0)) dy = -dy;
+            lcd.commit();
         }
-
-        t_next_upd += SCAN_INTERVAL_US;
-        gpio_set_dir(PIN_BARLED_SEG_BASE + seg, GPIO_IN);
-        seg = (seg < BARLED_NUM_SEGS - 1) ? seg + 1 : 0;
+        lcd.service();
     }
-}
-
-static void animate_bit_scan(uint64_t t_now_us) {
-    static int last_cursor = -1;
-    static uint8_t last_signal = 0;
-    static bool all0 = true;
-    static bool all1 = false;
-    static bool rise = false;
-    static bool fall = false;
-    static bool last_qty_ok = false;
-
-    const bool curr_qty_ok = glb_jjy_qty_percent >= 5;
-    if (!curr_qty_ok) {
-        uint8_t lvl = glb_jjy_qty_percent;
-        uint8_t c = ((t_now_us / 100000) % 10) < 5 ? 1 : 0;
-        for (int seg = 0; seg < BARLED_NUM_SEGS; seg++) {
-            vram[seg] = seg <= lvl ? c : 0;
-        }
-    }
-    else if (!last_qty_ok) {
-        for (int seg = 0; seg < BARLED_NUM_SEGS; seg++) {
-            vram[seg] = 0;
-        }
-    }
-    last_qty_ok = curr_qty_ok;
-    if (!curr_qty_ok) return;
-
-    int curr_cursor = (t_now_us / 100000) % BARLED_NUM_SEGS;
-    uint8_t curr_signal = glb_jjy_curr_signal;
-
-    if (curr_cursor != last_cursor) {
-        if (last_cursor >= 0) {
-            uint8_t c = 1;
-            if (all0) c = 0;
-            else if (all1) c = 7;
-            else if (rise && !fall) c = 7;
-            else if (!rise && fall) c = 0;
-            else if (rise && fall) c = 1;
-            vram[last_cursor] = c;
-        }
-        all0 = true;
-        all1 = true;
-        rise = false;
-        fall = false;
-        vram[curr_cursor] = 4;
-    }
-
-    if (curr_signal & ~last_signal) rise = true;
-    else if (~curr_signal & last_signal) fall = true;
-
-    if (curr_signal) all0 = false;
-    else all1 = false;
-
-    last_cursor = curr_cursor;
-    last_signal = curr_signal;
 }
--- src/ssd1309spi.hpp
+++ src/ssd1309spi.hpp
@@ -2,6 +2,7 @@
 #define SSD1309_SPI_HPP
 
 #include <stdint.h>
+#include <string.h>
 
 #include "hardware/gpio.h"
 #include "hardware/spi.h"
@@ -9,56 +10,90 @@
 #include "pico/stdlib.h"
 #include "pico/binary_info.h"
 
+namespace ssd1309spi {
+
+template<typename T>
+static inline T min(T a, T b) { return a < b ? a : b; }
+
+template<typename T>
+static inline T max(T a, T b) { return a > b ? a : b; }
+
+template<typename T>
+static inline T clip(T min, T max, T value) {
+    if (value < min) return min;
+    if (value > max) return max;
+    return value;
+}
+
+using seg_t = uint8_t;
+
+typedef enum : bool {
+    CMD = false,
+    DATA = true,
+} dc_t;
+
+typedef enum {
+    SET_MEM_MODE        = 0x20,
+    SET_COL_ADDR        = 0x21,
+    SET_PAGE_ADDR       = 0x22,
+    SET_HORIZ_SCROLL    = 0x26,
+    SET_SCROLL          = 0x2E,
+    SET_DISP_START_LINE = 0x40,
+    SET_CONTRAST        = 0x81,
+    SET_CHARGE_PUMP     = 0x8D,
+    SET_SEG_REMAP       = 0xA0,
+    SET_ENTIRE_ON       = 0xA4,
+    SET_ALL_ON          = 0xA5,
+    SET_NORM_DISP       = 0xA6,
+    SET_INV_DISP        = 0xA7,
+    SET_MUX_RATIO       = 0xA8,
+    SET_DISP            = 0xAE,
+    SET_COM_OUT_DIR     = 0xC0,
+    SET_COM_OUT_DIR_FLIP= 0xC0,
+    SET_DISP_OFFSET     = 0xD3,
+    SET_DISP_CLK_DIV    = 0xD5,
+    SET_PRECHARGE       = 0xD9,
+    SET_COM_PIN_CFG     = 0xDA,
+    SET_VCOM_DESEL      = 0xDB,
+} cmd_t;
+
+typedef enum : uint8_t {
+    BLACK = 0,
+    WHITE = 1,
+} pen_t;
+
 template<int W, int H, int SPI_INDEX, uint32_t SPI_FREQ, int PIN_RES_N, int PIN_CS_N, int PIN_DC, int PIN_SCLK, int PIN_MOSI>
-class Ssd1309Spi {
+class Lcd {
 public:
-    static constexpr int NUM_PAGES = H / 8;
+
+    static constexpr int PAGE_H = 8;
+    static constexpr int NUM_PAGES = (H + PAGE_H - 1) / PAGE_H;
+    static constexpr int NUM_PLANES = 2;
+    static constexpr int NUM_SEGS = W * NUM_PAGES;
 
     static_assert(
         (W == 128 && H == 64) ||
         (W == 128 && H == 32));
 
-    spi_inst_t* spi;
-    int dma_ch;
+private:
+    spi_inst_t* spi = NULL;
+    int dma_ch = 0;
     dma_channel_config dma_cfg;
+    seg_t front_buff[NUM_SEGS];
+    seg_t back_buff[NUM_SEGS];
 
-    uint8_t vram[W * H];
-
-    typedef enum : bool {
-        CMD = false,
-        DATA = true,
-    } dc_t;
-
-    typedef enum {
-        SET_MEM_MODE        = 0x20,
-        SET_COL_ADDR        = 0x21,
-        SET_PAGE_ADDR       = 0x22,
-        SET_HORIZ_SCROLL    = 0x26,
-        SET_SCROLL          = 0x2E,
-        SET_DISP_START_LINE = 0x40,
-        SET_CONTRAST        = 0x81,
-        SET_CHARGE_PUMP     = 0x8D,
-        SET_SEG_REMAP       = 0xA0,
-        SET_ENTIRE_ON       = 0xA4,
-        SET_ALL_ON          = 0xA5,
-        SET_NORM_DISP       = 0xA6,
-        SET_INV_DISP        = 0xA7,
-        SET_MUX_RATIO       = 0xA8,
-        SET_DISP            = 0xAE,
-        SET_COM_OUT_DIR     = 0xC0,
-        SET_COM_OUT_DIR_FLIP= 0xC0,
-        SET_DISP_OFFSET     = 0xD3,
-        SET_DISP_CLK_DIV    = 0xD5,
-        SET_PRECHARGE       = 0xD9,
-        SET_COM_PIN_CFG     = 0xDA,
-        SET_VCOM_DESEL      = 0xDB,
-    } cmd_t;
+    int curr_page = 0;
+    int num_sent_pages = 0;
 
 public:
-
-    Ssd1309Spi() : spi(SPI_INDEX == 0 ? spi0 : spi1) { }
+    Lcd() : spi(SPI_INDEX == 0 ? spi0 : spi1) { }
 
     void init() {
+        curr_page = 0;
+        num_sent_pages = NUM_PAGES;
+        memset(back_buff, 0x00, NUM_SEGS * sizeof(seg_t));
+        memset(front_buff, 0x00, NUM_SEGS * sizeof(seg_t));
+
         gpio_init(PIN_RES_N);
         gpio_init(PIN_CS_N);
         gpio_init(PIN_DC);
@@ -71,31 +106,28 @@ public:
         gpio_set_dir(PIN_CS_N, GPIO_OUT);
         gpio_set_dir(PIN_DC, GPIO_OUT);
 
-        // Enable SPI at 1 MHz and connect to GPIOs
         spi_init(spi, SPI_FREQ);
         gpio_set_function(PIN_SCLK, GPIO_FUNC_SPI);
         gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
 
-        // Make the SPI pins available to picotool
-        //bi_decl(bi_2pins_with_func(PICO_DEFAULT_SPI_RX_PIN, PIN_MOSI, PIN_SCLK, GPIO_FUNC_SPI));
-        // Make the CS pin available to picotool
+        //bi_decl(bi_2pins_with_func(PIN_MISO, PIN_MOSI, PIN_SCLK, GPIO_FUNC_SPI));
         //bi_decl(bi_1pin_with_name(PIN_CS_N, "SPI CS"));
+
+        sleep_ms(100); // check
+        gpio_put(PIN_RES_N, true);
+        sleep_ms(100); // check
 
         dma_ch = dma_claim_unused_channel(true);
         dma_cfg = dma_channel_get_default_config(dma_ch);
         channel_config_set_transfer_data_size(&dma_cfg, DMA_SIZE_8);
         channel_config_set_dreq(&dma_cfg, spi_get_dreq(spi, true));
         
-        sleep_ms(100); // check
-        gpio_put(PIN_RES_N, true);
-        sleep_ms(100); // check
-
-        write_cmd(cmd_t::SET_DISP);
+        set_disp(false);
         write_cmd(cmd_t::SET_MEM_MODE, 0);
         write_cmd(cmd_t::SET_DISP_START_LINE);
-        write_cmd((cmd_t)(cmd_t::SET_SEG_REMAP | 0x01));
+        write_cmd(cmd_t::SET_SEG_REMAP | 0x01);
         write_cmd(cmd_t::SET_MUX_RATIO, H - 1);
-        write_cmd((cmd_t)(cmd_t::SET_COM_OUT_DIR | 0x08));
+        write_cmd(cmd_t::SET_COM_OUT_DIR | 0x08);
         write_cmd(cmd_t::SET_DISP_OFFSET, 0);
         write_cmd(cmd_t::SET_COM_PIN_CFG, 
             (W == 128 && H == 32) ? 0x02 :
@@ -108,13 +140,98 @@ public:
         write_cmd(cmd_t::SET_NORM_DISP);
         write_cmd(cmd_t::SET_CHARGE_PUMP, 0x14);
         write_cmd(cmd_t::SET_SCROLL);
-        write_cmd((cmd_t)(cmd_t::SET_DISP | 0x01));
 
-        sleep_ms(100);
-        write_cmd(cmd_t::SET_COL_ADDR, 0, 7);
-        write_cmd(cmd_t::SET_PAGE_ADDR, 0, 0);
-        uint8_t test_data[] = { 0x55, 0xaa, 0x55, 0xaa, 0x55, 0xaa, 0x55, 0xaa };
-        write_blocking(dc_t::DATA, test_data, 8);
+        write_cmd(cmd_t::SET_COL_ADDR, 0, W - 1);
+        write_cmd(cmd_t::SET_PAGE_ADDR, 0, H - 1);
+        write_blocking(dc_t::DATA, front_buff, NUM_SEGS * sizeof(seg_t));
+
+        set_disp(true);
+    }
+
+    void set_disp(bool on) {
+        write_cmd(cmd_t::SET_DISP | (on ? 0x01 : 0x00));
+    }
+
+    void clear(seg_t c = 0x00) {
+        memset(back_buff, c, NUM_SEGS * sizeof(seg_t));
+    }
+
+    inline int get_seg_index(int x, int y) {
+        return (y / PAGE_H) * W + x;
+    }
+
+    void set_pixel(int x, int y, pen_t c = pen_t::WHITE) {
+        if (x < 0 || W <= x || y < 0 || H <= y) return;
+        uint8_t mask = 1 << (y % PAGE_H);
+        int iseg = get_seg_index(x, y);
+        switch (c) {
+        case pen_t::BLACK: back_buff[iseg] &= ~mask; break;
+        case pen_t::WHITE: back_buff[iseg] |= mask; break;
+        }
+    }
+
+    void fill_rect(int x, int y, int w, int h, pen_t c = pen_t::WHITE) {
+        int r = x + w, b = y + h;
+        x = clip(0, W, x);
+        y = clip(0, H, y);
+        r = clip(0, W, r);
+        b = clip(0, H, b);
+        w = r - x;
+        h = b - y;
+        if (w <= 0 || h <= 0) return;
+        
+        int first_page = y / PAGE_H;
+        int final_page = (b - 1) / PAGE_H;
+        seg_t first_seg = ~(seg_t)((1 << (y % PAGE_H)) - 1);
+        seg_t final_seg = (1 << (((b + PAGE_H - 1) % PAGE_H) + 1)) - 1;
+        
+        if (first_page == final_page) {
+            first_seg &= final_seg;
+        }
+        
+        for (int p = first_page; p <= final_page; p++) {
+            seg_t mask =
+                (p == first_page) ? first_seg :
+                (p == final_page) ? final_seg :
+                ~(seg_t)0;
+            seg_t *wr_ptr = &back_buff[p * W + x];
+
+            if (~mask == 0) {
+                memset(wr_ptr, c == pen_t::BLACK ? 0x00 : 0xff, w * sizeof(seg_t));
+            }
+            else if (c == pen_t::BLACK) {
+                mask = ~mask;
+                for (int i = 0; i < w; i++) {
+                    *(wr_ptr++) &= mask;
+                }
+            }
+            else {
+                for (int i = 0; i < w; i++) {
+                    *(wr_ptr++) |= mask;
+                }
+            }
+        }
+    }
+
+    void commit() {
+        memcpy(front_buff, back_buff, NUM_SEGS * sizeof(seg_t));
+        num_sent_pages = 0;
+    }
+
+    void service() {
+        if (num_sent_pages < NUM_PAGES) {
+            write_cmd(cmd_t::SET_COL_ADDR, 0, W - 1);
+            write_cmd(cmd_t::SET_PAGE_ADDR, curr_page, curr_page);
+            //write_dma_start(dc_t::DATA, front_buff + W * curr_page, sizeof(seg_t) * W);
+            write_blocking(dc_t::DATA, front_buff + W * curr_page, sizeof(seg_t) * W);
+            num_sent_pages += 1;
+            if (num_sent_pages < NUM_PAGES) {
+                curr_page = (curr_page + 1) % NUM_PAGES;
+            }
+            else {
+                curr_page = 0;
+            }
+        }
     }
 
     void write_cmd(const uint8_t cmd) {
@@ -132,41 +249,43 @@ public:
         write_blocking(dc_t::CMD, buf, 3);
     }
 
-    void write_blocking(const dc_t dc, const uint8_t *src, const uint32_t length) {
+    void write_blocking(const dc_t dc, const void *src, const size_t size_in_bytes) {
         write_dma_complete();
-        gpio_put(PIN_DC, dc);
-        spi_select();
-        spi_write_blocking(spi, src, length);
+        spi_select(dc);
+        spi_write_blocking(spi, (const uint8_t*)src, size_in_bytes);
         spi_deselect();
     }
 
-    void write_dma_start(const dc_t dc, const uint8_t *src, const uint32_t length) {
+    void write_dma_start(const dc_t dc, const void *src, const size_t size_in_bytes) {
         write_dma_complete();
-        dma_channel_configure(dma_ch, &dma_cfg,
-            &spi_get_hw(spi)->dr, // write address
-            src, // read address
-            length, // element count (each element is of size transfer_data_size)
-            false // don't start yet
-        ); 
-        gpio_put(PIN_DC, dc);
-        spi_select();
-        dma_channel_start(dma_ch);
+        spi_select(dc);
+        dma_channel_configure(dma_ch, &dma_cfg, &spi_get_hw(spi)->dr, src, size_in_bytes, true); 
     }
 
     void write_dma_complete() {
-        if (dma_channel_is_busy(dma_ch)) {
+        if (is_dma_busy()) {
             dma_channel_wait_for_finish_blocking(dma_ch);
         }
         spi_deselect();
     }
 
-    void spi_select() {
+    bool is_dma_busy() {
+        return dma_channel_is_busy(dma_ch);
+    }
+
+    void spi_select(dc_t dc) {
+        gpio_put(PIN_DC, dc);
         gpio_put(PIN_CS_N, false);
+        sleep_us(10);
     }
 
     void spi_deselect() {
+        sleep_us(10);
         gpio_put(PIN_CS_N, true);
+        sleep_us(10);
     }
 };
+
+}
 
 #endif
